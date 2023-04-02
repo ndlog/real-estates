78878d1e1d2029696778b0d34ae90ad166376f2a
Nguyen Duc Long <102477140+ndlog@users.noreply.github.com>
2023-04-02 21:57:57 +0700

    fix Banner, Search, and build Content #1

--- src/components/House.js
+++ src/components/House.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
-const House = () => {
-  return <div>House</div>;
+// import icons
+import { BiBed, BiBath, BiArea } from 'react-icons/bi';
+
+const House = ({ house }) => {
+    const { image, type, country, address, bedrooms, bathrooms, surface, price } = house;
+    return (
+        <div>
+            <img src={image} alt="" />
+        </div>
+    );
 };
 
 export default House;
--- src/components/HouseContext.js
+++ src/components/HouseContext.js
@@ -1,7 +1,69 @@
-import React from 'react';
+import React, { useState, useEffect, createContext } from 'react';
 
-const HouseContext = () => {
-  return <div>HouseContext</div>;
+// import data
+import { housesData } from '../data';
+
+// create context
+export const HouseContext = createContext();
+
+const HouseContextProvider = ({ children }) => {
+    const [houses, setHouses] = useState(housesData);
+    const [country, setCountry] = useState('Location (any)');
+    const [countries, setCountries] = useState([]);
+    const [property, setProperty] = useState('Property type (any)');
+    const [properties, setProperties] = useState([]);
+    const [price, setPrice] = useState('Price range (any)');
+    const [loading, setLoading] = useState(false);
+
+    // return all countries
+    useEffect(() => {
+        const allCountries = houses.map((house) => {
+            return house.country;
+        });
+
+        // remove duplicates
+        const uniqueCountries = ['Location (any)', ...new Set(allCountries)];
+
+        // set countries state
+        setCountries(uniqueCountries);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    // return all properties
+    useEffect(() => {
+        const allProperties = houses.map((house) => {
+            return house.type;
+        });
+
+        // remove duplicates
+        const uniqueProperties = ['Location (any)', ...new Set(allProperties)];
+
+        // set properties state
+        setProperties(uniqueProperties);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    const handleClick = () => {};
+
+    return (
+        <HouseContext.Provider
+            value={{
+                country,
+                setCountry,
+                countries,
+                property,
+                setProperty,
+                properties,
+                price,
+                setPrice,
+                houses,
+                loading,
+                handleClick,
+            }}
+        >
+            {children}
+        </HouseContext.Provider>
+    );
 };
 
-export default HouseContext;
+export default HouseContextProvider;
