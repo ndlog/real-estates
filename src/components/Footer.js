89315c522b4a4baa6426aee98ccdad750770172f
Nguyen Duc Long <102477140+ndlog@users.noreply.github.com>
2023-04-04 15:41:02 +0700

    build Content #end

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Footer = () => {
-  return <div>Footer</div>;
+    return (
+        <footer className="bg-black py-8 text-center text-white">
+            <div className="container mx-auto">Copyright &copy; 2023. All rights reserved</div>
+            <div className="container mx-auto">by Nguyen Duc Long</div>
+        </footer>
+    );
 };
 
 export default Footer;
